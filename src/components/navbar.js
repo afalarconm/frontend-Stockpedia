8402b961f65d2b84d34d8694ace7e5aede37bace
afalarconm <afalarconm@uc.cl>
2023-09-21 08:50:38 -0300

    added user authentication to buy button

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -23,11 +23,19 @@ const LogoutButton = () => {
 };
 
 function Barra() {
-  const [isOpen, setIsOpen] = useState(false);    
-  const {user, isAuthenticated, isLoading} = useAuth0();
+  const [isOpen, setIsOpen] = useState(false);
+  const { user, isAuthenticated, isLoading } = useAuth0();
   const toggle = () => setIsOpen(!isOpen);
   if (isLoading) {
-    return <div>Loading ...</div>;
+    return (<nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="/" className="flex items-center">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Emoji_u1f310.svg/192px-Emoji_u1f310.svg.png" className="h-8 mr-3" alt="Stockpedia Logo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Stockpedia</span>
+        </a>
+        <a className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Loading ...</a>;
+      </div>
+    </nav>);
   }
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
@@ -37,7 +45,7 @@ function Barra() {
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Stockpedia</span>
         </a>
         <div className="flex md:order-2">
-          {isAuthenticated ? <LogoutButton/> : <LoginButton />}
+          {isAuthenticated ? <LogoutButton /> : <LoginButton />}
           <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
             <span className="sr-only">Abrir el menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -51,9 +59,9 @@ function Barra() {
               <a href="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
             </li>
             {isAuthenticated && (
-            <li>
-              <a href="/profile" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Mi perfil</a>
-            </li> )}
+              <li>
+                <a href="/profile" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Mi perfil</a>
+              </li>)}
             <li>
               <a href="/" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
             </li>
