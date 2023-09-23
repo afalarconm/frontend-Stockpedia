844f764fd26c3bb57ce99a347f1f9decf1b53041
afalarconm <afalarconm@uc.cl>
2023-09-23 01:36:44 -0300

    fixed a couple of details

--- src/pages/StockDetailsPage.js
+++ src/pages/StockDetailsPage.js
@@ -103,7 +103,7 @@ const StockDetailsPage = () => {
                                                 <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M1 12c1.5 1.5 5.25 3 9 3s7.5-1.5 9-3m-9-1h.01M2 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1ZM14 5V3a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v2h8Z"></path>
                                             </svg>
                                         </div>
-                                        <input type="text" id="quantity" className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-8 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="1"></input>
+                                        <input type="text" id="quantity" className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-8 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0"></input>
                                     </div>
                                 </div>
                                 <div className="mb-2">
